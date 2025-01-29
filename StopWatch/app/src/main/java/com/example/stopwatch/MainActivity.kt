5bc2e7e949a036a10d2474351dd0d081322c4faa
Foeva1 <142251952+Foeva1@users.noreply.github.com>
2025-01-29 21:52:35 +0700

    l2v4
    Смена ориентации и сворачивание актвиности

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
@@ -11,6 +12,36 @@ class MainActivity : AppCompatActivity() {
     lateinit var stopwatch:Chronometer
     var running      = false
     var offset: Long = 0
+
+    val OFFSET_KEY   = "offset"
+    val RUNNING_KEY  = "running"
+    val BASE_KEY     = "base"
+
+    //Схороняем переменнын
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY,offset)
+        savedInstanceState.putBoolean(RUNNING_KEY,running)
+        savedInstanceState.putLong(BASE_KEY,stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (running){
+            saveOffset()
+            stopwatch.stop()
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        if(running){
+            setBaseTime()
+            stopwatch.start()
+            offset = 0
+        }
+    }
+
 
     //Обновляем stopwatch.base
     fun setBaseTime(){
@@ -28,6 +59,18 @@ class MainActivity : AppCompatActivity() {
 
         stopwatch       = findViewById<Chronometer>(R.id.stopwatch)
 
+        //Загрузка состояний
+        if (savedInstanceState != null){
+            offset  = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running){
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()}
+            else
+                setBaseTime()
+        }
+
+        //----Кнопки
         //Запускаем секундомер
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener{
