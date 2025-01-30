f5fc1058847b530ae3ce3cdd519e68df3b447fe8
Foeva1 <142251952+Foeva1@users.noreply.github.com>
2025-01-30 20:14:03 +0700

    l3v1
    Создан пустой макет

---
+++ CoroutineDemo/app/src/main/java/com/example/coroutinedemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.coroutinedemo
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.ui.AppBarConfiguration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+
+
+}
