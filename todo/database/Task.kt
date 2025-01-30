29097b393ed2c7fb53c726095d4cbe7f4e97fc4c
Foeva1 <142251952+Foeva1@users.noreply.github.com>
2025-01-30 21:13:13 +0700

    l4v4
    Определение структуры и функций базы

---
+++ todo/database/Task.kt
@@ -0,0 +1,9 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val title: String,
+    val priority: Int
+)
